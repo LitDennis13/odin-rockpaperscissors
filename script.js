9851fe4da6d3672ccb9e24a270892df9e2724b57
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2023-11-01 21:10:39 -0600

    Add the computerchoice function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let random_number = Math.floor(Math.random()*3);
+    let decision = choices[random_number];
+    return decision;
+}
+
+
+
+/*
+
+an array called choices that contains rock, paper, and scissors
+
+function getComputerChoice
+    a variable that makes a random number
+    a variable called decision that uses the random number to get a value from the choices array
+    return the computer_choice value
+
+
+function runGame
+    a variable called computer
+
+
+
+
+*/
