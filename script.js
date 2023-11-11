31305418d8693cbbb48a62f1a591bbc9cec0f4c9
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2023-11-10 20:28:49 -0600

    re-order and remove functionality
    checkifinarray function was removed
    original game loop removed
    prompt and alerts were removed

--- script.js
+++ script.js
@@ -6,30 +6,9 @@ function getComputerChoice() {
     return decision;
 }
 
-function checkIfInArray(value_to_check,array) {
-    for (let i = 0; i < array.length; i++) {
-        if (value_to_check == array[i]) {
-            return true;
-        }
-    }
-    return false;
 
-}
 
-function getPlayerChoice() {
-    let got_player_choice = false;
-    let player_choice = "";
 
-    while (!got_player_choice) {
-        player_choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        if (checkIfInArray(player_choice,choices)) {
-            return player_choice;
-        }
-        else {
-            alert("Please pick Rock, Paper, or Scissors")
-        }
-    }
-}
 
 function playRound(computer_choice, player_choice) {
     if (computer_choice === player_choice){
@@ -58,17 +37,10 @@ function playRound(computer_choice, player_choice) {
     }
 }
 
-let rounds = 5;
-let rounds_played = 0;
-let player_choice;
-let computer_choice;
-let round_result;
-while (rounds_played < rounds) {
-    player_choice = getPlayerChoice();
-    computer_choice = getComputerChoice();
-    round_result = playRound(computer_choice, player_choice);
-    rounds_played++;
-    alert(`You chose: ${player_choice}\nComputer chose: ${computer_choice}\n\n${round_result}\nRound Number: ${rounds_played}`);
+function getPlayerChoice() {
     
 }
+
+
+
 
